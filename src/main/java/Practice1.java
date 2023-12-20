7ed0de9967614fee8edc1837e55d776b0a32b63f
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-12-20 12:58:22 -0800

    Added new coding problem and accompanying tests

--- src/main/java/Practice1.java
+++ src/main/java/Practice1.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Practice1 {
     /**
      * Given an integer, check to see if it's a palindrome (reads the same forward and backward)
@@ -15,5 +18,33 @@ public class Practice1 {
             forward/= 10;
         }
         return backward == x;
+    }
+
+    /*
+    Given an array of integers nums and an integer target, return indices of the two numbers
+    such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+     */
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] solution = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        //put the array and indices into a map
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            //take the target, subtract the number at the index
+            int complement = target - nums[i];
+            //check to see if it's anywhere else in the map
+            if(map.containsKey(complement) && map.get(complement) != i){
+                solution[0] = map.get(complement);
+                solution[1] = i;
+            }
+        }
+        //if there is no solution, it will return [0, 0]
+        return solution;
     }
 }
--- src/test/java/Practice1Test.java
+++ src/test/java/Practice1Test.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Practice1Test {
 
     @Test
-    void isPalindrome() {
+    void isPalindromeTest() {
         int testPalendrome1 = 121;
         int testPalendrome2 = 345;
         int testPalendrome3 = -121;
@@ -18,4 +20,21 @@ class Practice1Test {
         assertTrue(Practice1.isPalindrome(testPalendrome4));
         assertTrue(Practice1.isPalindrome(testPalendrome5));
     }
+
+    @Test
+    void twoSumTest(){
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+
+        int[] nums3 = {1, 7, 11, 15};
+        int target3 = 9;
+
+        assertEquals("[0, 1]", Arrays.toString(Practice1.twoSum(nums1, target1)) );
+        assertEquals("[1, 2]", Arrays.toString(Practice1.twoSum(nums2, target2)) );
+        assertEquals("[0, 0]", Arrays.toString(Practice1.twoSum(nums3, target3)) );
+    }
+
 }
